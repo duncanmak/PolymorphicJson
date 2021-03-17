8fdc92591dde4f6d86a5058a24c2f1d9b7e79be9
Duncan Mak <duncanmak@gmail.com>
2021-03-17 16:17:24 -0400

    Cleanup

--- Converter.cs
+++ Converter.cs
@@ -1,45 +1,40 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 #nullable enable
 
 public class BaseConverter : JsonConverter<IBase> {
 
-    public static Dictionary<string, Type> mapping = new() {
-        ["Foo"] = typeof(Foo),
-        ["Bar"] = typeof(Bar)
-    };
+    public static Dictionary<string, Type> mapping = new();
 
-    public override bool CanConvert(Type type) => type.IsAssignableFrom(typeof(IBase));
+    public BaseConverter()
+    {
+        foreach (var t in Assembly.GetExecutingAssembly().GetTypes()) {
+            if (t.IsAssignableTo(typeof(IBase))) {
+                mapping[t.Name] = t;
+            }
+        }
+    }
+
+    public override bool CanConvert(Type t) => t.IsAssignableFrom(typeof(IBase));
 
     public override IBase? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        return ReadBase(ref reader, typeToConvert, options);
-    }
-
-    public IBase? ReadFoo(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        return (IBase?) JsonSerializer.Deserialize(ref reader, typeof(Foo), options);
-    }
-
-    public IBase? ReadBase(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var r = reader;
 
         // Peek into the "type" attribute
-        if (r.TokenType != JsonTokenType.StartObject) throw new JsonException();
+        if (r.TokenType != JsonTokenType.StartObject) throw new JsonException("JsonTokenType.StartObject");
 
-        r.Read();
-        if (r.TokenType != JsonTokenType.PropertyName) throw new JsonException();
+        if (!r.Read() || r.TokenType != JsonTokenType.PropertyName) throw new JsonException("JsonTokenType.PropertyName");
 
-        if (r.GetString() != "type") throw new JsonException();
+        if (r.GetString() != "type") throw new JsonException("type");
 
-        r.Read();
-        if (r.TokenType != JsonTokenType.String) throw new JsonException();
+        if (!r.Read() || r.TokenType != JsonTokenType.String) throw new JsonException("JsonTokenType.String");
 
-        var t = r.GetString();
-        mapping.TryGetValue(t ?? "", out Type? type);
+        var t = r.GetString(); mapping.TryGetValue(t ?? "", out Type? type);
+
         if (type == null) throw new JsonException($"{t} is not a recognized type");
 
         return (IBase?) JsonSerializer.Deserialize(ref reader, type, options);
@@ -48,7 +43,6 @@ public class BaseConverter : JsonConverter<IBase> {
     public override void Write(Utf8JsonWriter writer, IBase value, JsonSerializerOptions options)
     {
         var type = value.GetType();
-        if (type != null)
-            JsonSerializer.Serialize(writer, value, type, options);
+        if (type != null) JsonSerializer.Serialize(writer, value, type, options);
     }
 }
